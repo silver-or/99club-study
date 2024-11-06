f122ca38f5c70306a73b6f011937c09fc4889308
silver-or <laura817@naver.com>
2024-11-06 15:53:10 +0900

    10일차 문제

--- 2st_week/baekjoon_9933.py
+++ 2st_week/baekjoon_9933.py
@@ -3,10 +3,12 @@ words = []
 answer = ''
 for _ in range(N):
     words.append(input())
-# print(words) # ['las', 'god', 'psala', 'sal']
+
 for word in words:
     rWord = word[::-1]
     if rWord in words:
         answer = rWord
-mIndex = int(len(answer)/2)
+        break
+
+mIndex = len(answer) // 2
 print(len(answer), answer[mIndex])
