4c8047b998cb362fbef0b8c398d8485bbd35664b
silver-or <laura817@naver.com>
2024-11-22 17:03:25 +0900

    26일차 문제 수정

--- week4/baekjoon_11004.py
+++ week4/baekjoon_11004.py
@@ -1,15 +1,15 @@
 import sys
 
-def solution(N, K, arr):
+def solution(K, arr):
     answer = 0
     arr.sort()
     answer = arr[K-1]
     return answer
 
 def main():
-    N, K = map(int, sys.stdin.readline().split())
+    _, K = map(int, sys.stdin.readline().split())
     arr = list(map(int, sys.stdin.readline().split()))
-    print(solution(N, K, arr))
+    print(solution(K, arr))
 
 if __name__ == '__main__':
     main()
