6683a3acf529dc91ff20c57914caae7375960bd9
silver-or <laura817@naver.com>
2024-11-26 00:20:24 +0900

    29일차 문제 수정

--- week5/leet_2231.py
+++ week5/leet_2231.py
@@ -1,28 +1,21 @@
 class Solution:
-    def __init__(self):
-        self.answer = 0
-
     def largestInteger(self, num: int) -> int:
         nums = list(map(int, str(num)))
-        odds, evens = [], []
+        odds = [n for n in nums if n % 2 != 0]
+        evens = [n for n in nums if n % 2 == 0]
 
-        for n in nums:
-            if n % 2 == 0:
-                evens.append(n)
-            else:
-                odds.append(n)
-
+        # 내림차순으로 정렬
         odds.sort(reverse=True)
         evens.sort(reverse=True)
 
+        # 교환하며 리스트를 재구성
         for i in range(len(nums)):
-            if nums[i] % 2 == 0:
-                nums[i] = evens.pop(0)  # 가장 큰 짝수로 교체
-            else:
-                nums[i] = odds.pop(0)  # 가장 큰 홀수로 교체
+            if nums[i] % 2 == 0:  # 짝수
+                nums[i] = evens.pop(0)  # 가장 큰 짝수 사용
+            else:  # 홀수
+                nums[i] = odds.pop(0)  # 가장 큰 홀수 사용
 
-        self.answer = int(''.join(map(str, nums)))
-        return self.answer
+        return int(''.join(map(str, nums)))
 
 def main():
     print(Solution().largestInteger(1234))
