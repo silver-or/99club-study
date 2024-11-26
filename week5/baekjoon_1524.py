ff86029f8960ced123a7fff2324e65358ceeb94b
silver-or <laura817@naver.com>
2024-11-26 16:49:36 +0900

    30일차 문제

---
+++ week5/baekjoon_1524.py
@@ -0,0 +1,22 @@
+import sys
+
+def solution(T):
+    for _ in range(T):
+        sys.stdin.readline()
+        N, M = map(int, sys.stdin.readline().split())
+        sejun = sorted(list(map(int, sys.stdin.readline().split())))
+        sebi = sorted(list(map(int, sys.stdin.readline().split())))
+
+        while sejun and sebi:
+            if sejun[0] < sebi[0]:
+                sejun.pop(0)
+            else: # sejun[0] >= sebi[0]
+                sebi.pop(0)
+
+        print('S') if sejun else print('B')
+
+def main():
+    solution(int(sys.stdin.readline()))
+
+if __name__ == '__main__':
+    main()
